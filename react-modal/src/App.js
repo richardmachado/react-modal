857877173381d017f4c15fcf0e03341d4321bce6
Richard Machado <mmachado.richard@gmail.com>
2020-09-30 11:32:56 -0700

    practiced modals

---
+++ react-modal/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import Modals from "./Modal";
+
+function App() {
+  const modalRef = React.useRef();
+
+  const openModal = () => {
+    modalRef.current.openModal()
+  };
+
+  return (
+    <div className="App">
+      <button className={'push_button red'} onClick={openModal}>Submit a new Request</button>
+      <Modals ref={modalRef}>
+        <h1>Modal Header</h1>
+        <p>
+          <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi eligendi esse facere illo in minima nulla quis reiciendis. Eligendi impedit nostrum quam quod reprehenderit, ullam veritatis. Fuga provident quos velit.</span><span>Accusantium ad, alias animi et eum, excepturi explicabo fuga iusto magnam maxime minima molestias nam nemo nostrum pariatur perspiciatis porro quae quibusdam quidem quis repudiandae sed ullam vel, veniam vero.</span><span>Eligendi nulla quasi quibusdam quod saepe suscipit tenetur voluptas voluptate! Accusamus amet, commodi culpa distinctio dolor eveniet expedita hic iure magnam magni mollitia nulla officia quas, reiciendis repellat sapiente, veniam!</span>
+        </p>
+
+
+        <button onClick={() => modalRef.current.close()}>
+          Close Modal
+        </button>
+      </Modals>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-modal/src/BasicModal.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import Modal from 'react-modal';
+
+import './App.css';
+
+Modal.setAppElement('#root')
+function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  return (
+    <div className="App">
+      <button onClick={() => setModalIsOpen(true)}>Open Modal</button>
+      {/* onRequestClose allows you to close the modal by clicking outside the box or the escape key , you can also add shouldCloseOnOverlayClick={false} to override the overlay and only close it with the escape key */}
+      <Modal isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={
+   {       overlay: {
+     backgroundColor: 'grey'
+          },
+            content: {
+       color:'orange'
+     }
+      }
+    }
+      >
+        <h2>Modal</h2>
+        <p>Body</p>
+        <button onClick={()=>setModalIsOpen(false)}>Close</button>
+     </Modal>
+    </div>
+  );
+}
+
+export default App;
