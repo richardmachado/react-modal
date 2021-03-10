f82c53cd7f808d77488171a48695b79374e72aa5
Richard Machado <mmachado.richard@gmail.com>
2021-03-10 09:31:34 -0800

    prettier

--- src/BasicModal.js
+++ src/BasicModal.js
@@ -1,31 +1,31 @@
-import React, {useState} from 'react';
-import Modal from 'react-modal';
+import React, { useState } from "react";
+import Modal from "react-modal";
 
-import './App.css';
+import "./App.css";
 
-Modal.setAppElement('#root')
+Modal.setAppElement("#root");
 function App() {
-  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   return (
     <div className="App">
       <button onClick={() => setModalIsOpen(true)}>Open Modal</button>
       {/* onRequestClose allows you to close the modal by clicking outside the box or the escape key , you can also add shouldCloseOnOverlayClick={false} to override the overlay and only close it with the escape key */}
-      <Modal isOpen={modalIsOpen}
+      <Modal
+        isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
-        style={
-   {       overlay: {
-     backgroundColor: 'grey'
+        style={{
+          overlay: {
+            backgroundColor: "grey",
           },
-            content: {
-       color:'orange'
-     }
-      }
-    }
+          content: {
+            color: "orange",
+          },
+        }}
       >
         <h2>Modal</h2>
         <p>Body</p>
-        <button onClick={()=>setModalIsOpen(false)}>Close</button>
-     </Modal>
+        <button onClick={() => setModalIsOpen(false)}>Close</button>
+      </Modal>
     </div>
   );
 }
--- src/Modal.js
+++ src/Modal.js
@@ -1,38 +1,35 @@
-import React, {forwardRef, useImperativeHandle, useState} from "react";
+import React, { forwardRef, useImperativeHandle, useState } from "react";
 import ReactDOM from "react-dom";
 
 const Modal = forwardRef((props, ref) => {
+  const [display, setDisplay] = useState(true);
 
-
-    const [display, setDisplay] = useState(true);
-  
-    useImperativeHandle(ref, () => {
-      return {
-        openModal: () => open(),
-        close: () => close()
-      }
-    });
-  
-    const open = () => {
-      setDisplay(true)
+  useImperativeHandle(ref, () => {
+    return {
+      openModal: () => open(),
+      close: () => close(),
     };
-  
-    const close = () => {
-      setDisplay(false);
-    };
-  
-    if (display) {
-      return ReactDOM.createPortal(
-        <div className={"modal-wrapper"}>
-          <div onClick={close} className={"modal-backdrop"} />
-          <div className={"modal-box"}>
-            {props.children}
-          </div>
-        </div>, document.getElementById("modal-root"))
-    }
-  
-    return null;
-  
   });
-  
-  export default Modal
+
+  const open = () => {
+    setDisplay(true);
+  };
+
+  const close = () => {
+    setDisplay(false);
+  };
+
+  if (display) {
+    return ReactDOM.createPortal(
+      <div className={"modal-wrapper"}>
+        <div onClick={close} className={"modal-backdrop"} />
+        <div className={"modal-box"}>{props.children}</div>
+      </div>,
+      document.getElementById("modal-root")
+    );
+  }
+
+  return null;
+});
+
+export default Modal;
